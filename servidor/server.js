9d036b6063515ea91369ba3d4516db03db94aa59
Fabi <fabicaneyu@gmail.com>
2020-09-26 23:50:30 -0300

    Finalizado Json 10 mulheres

--- servidor/server.js
+++ servidor/server.js
@@ -1,19 +1,19 @@
 const express = require('express')
-const router = express.Router()
+const app = express();
 const porta = 3000;
 const biografias = require('../mulheres/biografia.json')
 
 //@route GET /biografias
 //@desc Biografias 
 //@access Public
-router.get('/biografias', (req, res) => {
+app.get('/biografias', (req, res) => {
     res.json(biografias);
 })
 
 //@route GET /biografias/:id
 //@desc Retorna uma biografia
 //@access Public
-router.get('/biografias/:id', (req,res)=> {
+app.get('/biografias/:id', (req,res)=> {
     const {id} = req.params;
     const biografia = biografias.find(bio => bio.id == id);
 
@@ -22,9 +22,7 @@ router.get('/biografias/:id', (req,res)=> {
     res.json(biografia);
 })
 
-/* const id = req.params.id;
-    const mensagem = mensagens[id]
-    res.json(mensagem); */
+
 
 app.listen(porta, function() {
     console.log("Servidor rodando")
