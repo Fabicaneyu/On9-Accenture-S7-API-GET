fa4d48e6fed0e2821327a2602b7a8da84085285b
Fabi <fabicaneyu@gmail.com>
2020-09-26 23:16:56 -0300

    adicionado router

--- servidor/server.js
+++ servidor/server.js
@@ -3,12 +3,16 @@ const app = express();
 const porta = 3000;
 const biografias = require('../mulheres/biografia.json')
 
-//Get
+//@route GET /biografias
+//@desc Biografias 
+//@access Public
 app.get('/biografias', (req, res) => {
     res.json(biografias);
 })
 
-//Get ID
+//@route GET /biografias/:id
+//@desc Retorna uma biografia
+//@access Public
 app.get('/biografias/:id', (req,res)=> {
     const id = req.params.id;
     const mensagem = biografias[id];
